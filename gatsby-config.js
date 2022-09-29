4d79492bf5c882bf21d472e5ad3c68fac2a08576
Bailey Latimer <latimerdesignco@gmail.com>
2022-09-29 15:46:26 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: `dyode-frontend-challenge`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-shopify',
+    options: {
+      "shopName": "dyode-frontend-challenge",
+      "accessToken": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
