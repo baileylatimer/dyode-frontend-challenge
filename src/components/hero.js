5c5ad8e03ca394acc811d78945bcf9f854437a0c
Bailey Latimer <latimerdesignco@gmail.com>
2022-10-04 15:39:58 -0700

    added hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Image from './image';
-
+import { StaticImage } from "gatsby-plugin-image"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 const settings = {
-  dots: false,
+  dots: true,
   infinite: true,
   autoplay: true,
 }
@@ -15,11 +14,36 @@ const settings = {
 const Hero = ({ data }) => {
   return (
     <Slider {...settings} className="overflow-hidden">
-          <div>
-            <Image src="hero.jpg" alt="Sunset"/>
-            <img src="./logo.png"/>
-            <h1>1</h1>
+      <div className="slide flex justify-center items-center">
+        <StaticImage className="slider--img" src="../images/hero--01.jpg" alt="Shop New Arrivals" />
+        <div className="container">
+          <div className="slider--content flex flex-col">
+            <h1 className="m0">Shop New Arrivals</h1>
+            <p className="subheader py-2">Our coolest new items are waiting for you!</p>
+            <a href="/test" className="btn btn--primary">Shop Now</a>
           </div>
+        </div>
+      </div>
+      <div className="slide flex justify-center items-center">
+        <StaticImage className="slider--img" src="../images/hero--02.jpg" alt="Our Fave Tees" />
+        <div className="container">
+          <div className="slider--content flex flex-col">
+            <h1 className="m0 text--light">Our Fave Tees</h1>
+            <p className="subheader py-2 text--light">Shop all of our favorites.</p>
+            <a href="/test" className="btn btn--primary">Shop Now</a>
+          </div>
+        </div>
+      </div>
+      <div className="slide flex justify-center items-center">
+        <StaticImage className="slider--img" src="../images/hero--03.jpg" alt="Men’s Tees" />
+        <div className="container">
+          <div className="slider--content flex flex-col">
+            <h1 className="m0 text--light">Men’s Tees</h1>
+            <p className="subheader py-2 text--light">Find the perfect shirt.</p>
+            <a href="/test" className="btn btn--primary">Shop Now</a>
+          </div>
+        </div>
+      </div>
 
     </Slider>
   )
